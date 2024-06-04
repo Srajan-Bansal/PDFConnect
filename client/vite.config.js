cbedd487506c37b45639af36169f662fb0a6ea83
Srajan Bansal <srajanbansal999@gmail.com>
2024-06-04 21:59:11 +0530

    Monitor the app using prometheus + Grafana stack

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: { chunkSizeWarningLimit: 1600 },
+	publicDir: 'public',
 	server: {
-		// Specify the public directory as the root
-		base: './public',
-		// host: '0.0.0.0',
-		// port: 5173,
+		host: '0.0.0.0',
+		port: 5173,
 	},
 });
