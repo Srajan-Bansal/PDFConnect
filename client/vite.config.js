fe86b9dd2b090e22f69daac3ab73b1a245686d93
Srajan Bansal <srajanbansal999@gmail.com>
2024-06-12 19:09:08 +0530

    bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-// Convert import.meta.url to a file path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Manually load environment variables from config.env
-dotenv.config({ path: resolve(__dirname, './config.env') });
 
 // https://vitejs.dev/config/
 export default defineConfig({
