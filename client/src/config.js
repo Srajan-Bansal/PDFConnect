e9d2b62cbbfca8097f3dc96af6b6dd76db5b37da
Srajan Bansal <87896128+Srajan-Bansal@users.noreply.github.com>
2024-06-12 19:14:45 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // config.js
-const baseURL = 'https://pdfconnect-server.onrender.com/';
+const baseURL = 'https://pdfconnect-server.onrender.com';
 // const MobileURL = 'http://192.168.1.9:8000';
 const config = {
 	userAPI: `${baseURL}/api/v1/user`,
