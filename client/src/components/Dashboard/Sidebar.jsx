5f5795ecb15f6661cc6892eecce88e413c80ce0e
Srajan Bansal <srajanbansal999@gmail.com>
2024-07-08 23:48:47 +0530

    minor changes

--- client/src/components/Dashboard/Sidebar.jsx
+++ client/src/components/Dashboard/Sidebar.jsx
@@ -1,4 +1,12 @@
+import useLogout from "../../hooks/useLogout";
+
 const Sidebar = () => {
+    const { logout } = useLogout();
+
+    async function handleLogOut() {
+        await logout();
+    }
+
     return (
         <div className="w-1/4 bg-gray-800 text-white flex flex-col items-center p-4">
             <div className="mb-8">
@@ -11,7 +19,7 @@ const Sidebar = () => {
             <button className="mb-4 py-2 px-6 bg-blue-600 rounded hover:bg-blue-500">
                 Update Password
             </button>
-            <button className="mb-4 py-2 px-6 bg-red-600 rounded hover:bg-red-500">
+            <button className="mb-4 py-2 px-6 bg-red-600 rounded hover:bg-red-500" onClick={handleLogOut}>
                 Logout
             </button>
             <button className="py-2 px-6 bg-red-800 rounded hover:bg-red-700">
