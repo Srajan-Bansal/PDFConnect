a79de41b6bd7b5493c37ff6b2396ecf0bb2a569a
Srajan Bansal <srajanbansal999@gmail.com>
2024-08-21 00:01:56 +0530

    dependency change

--- client/src/components/Dashboard/SideBar/SideBar.jsx
+++ client/src/components/Dashboard/SideBar/SideBar.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './SideBar.css'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,24 +6,24 @@ import { faCircleInfo, faKey, faRightFromBracket, faTrash } from '@fortawesome/f
 function SideBar() {
 
   return (
-      <div className="left">
-        <Link to="userDetails" className="s1">
-          <FontAwesomeIcon className='img' icon={faCircleInfo} />
-          <span className='para'>User Details</span>
-        </Link>
-        <Link to="updatePassword" className="s1">
-          <FontAwesomeIcon className='img' icon={faKey} />
-          <span className='para'>Update Password</span>
-        </Link>
-        <Link to="logout" className="s1">
-          <FontAwesomeIcon className='img' icon={faRightFromBracket} />
-          <span className='para'>Logout</span>
-        </Link>
-        <Link to="deleteAccount" className="s1">
-          <FontAwesomeIcon className='img' icon={faTrash} />
-          <span className='para'>Delete Account</span>
-        </Link>
-      </div>
+    <div className="left">
+      <Link to="userDetails" className="s1">
+        <FontAwesomeIcon className='img' icon={faCircleInfo} />
+        <span className='para'>User Details</span>
+      </Link>
+      <Link to="updatePassword" className="s1">
+        <FontAwesomeIcon className='img' icon={faKey} />
+        <span className='para'>Update Password</span>
+      </Link>
+      <Link to="logout" className="s1">
+        <FontAwesomeIcon className='img' icon={faRightFromBracket} />
+        <span className='para'>Logout</span>
+      </Link>
+      <Link to="deleteAccount" className="s1">
+        <FontAwesomeIcon className='img' icon={faTrash} />
+        <span className='para'>Delete Account</span>
+      </Link>
+    </div>
   )
 }
 
