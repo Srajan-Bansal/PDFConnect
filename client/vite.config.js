c92164ab26e0d6b86a2498ad375baffc082cb46a
Srajan Bansal <srajanbansal999@gmail.com>
2025-03-29 00:57:11 +0530

    changes in workflow

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		outDir: 'build',
+		outDir: 'dist',
 		chunkSizeWarningLimit: 2000,
 		rollupOptions: {
 			output: {
